63f685b41b3ec4dc03865e2b31db502b8ef04b5d
Hasalam <markshykh@gmail.com>
2022-01-26 18:21:33 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bArt_Test_task.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
