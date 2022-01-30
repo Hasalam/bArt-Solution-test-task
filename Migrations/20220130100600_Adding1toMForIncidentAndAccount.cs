f85050ead80d4159984c55d9fe470ef76266911b
Hasalam <markshykh@gmail.com>
2022-01-30 12:43:08 +0200

    Added models and login page

---
+++ Migrations/20220130100600_Adding1toMForIncidentAndAccount.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace bArt_Test_task.Migrations
+{
+    public partial class Adding1toMForIncidentAndAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IncidentId",
+                table: "Account",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_IncidentId",
+                table: "Account",
+                column: "IncidentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Account_Incident_IncidentId",
+                table: "Account",
+                column: "IncidentId",
+                principalTable: "Incident",
+                principalColumn: "IncidentName",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Account_Incident_IncidentId",
+                table: "Account");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Account_IncidentId",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "IncidentId",
+                table: "Account");
+        }
+    }
+}
